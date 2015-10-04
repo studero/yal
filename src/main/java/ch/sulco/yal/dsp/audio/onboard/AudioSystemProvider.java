6c5a235d41164e176d6635fa16acf30be984f6d0
Roman Studer <mail@romanstuder.ch>
2015-10-04 15:55:07 +0200

    fix audio system provider

--- src/main/java/ch/sulco/yal/dsp/audio/onboard/AudioSystemProvider.java
+++ src/main/java/ch/sulco/yal/dsp/audio/onboard/AudioSystemProvider.java
@@ -1,5 +1,8 @@
 package ch.sulco.yal.dsp.audio.onboard;
 
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
@@ -20,31 +23,29 @@ import javax.sound.sampled.UnsupportedAudioFileException;
 import ch.sulco.yal.dsp.AppConfig;
 import ch.sulco.yal.dsp.audio.ChannelInfo;
 
+@Singleton
 public class AudioSystemProvider {
-	private final AppConfig appConfig;
-	
-	public AudioSystemProvider(AppConfig appConfig) {
-		this.appConfig = appConfig;
-	}
+
+	@Inject
+	private AppConfig appConfig;
 
 	public Clip getClip(AudioFormat format, byte[] data, int offset, int bufferSize) throws LineUnavailableException {
-		if(format == null){
-			format = appConfig.getAudioFormat();
+		if (format == null) {
+			format = this.appConfig.getAudioFormat();
 		}
 		Clip clip = AudioSystem.getClip();
 		clip.open(format, data, offset, bufferSize);
 		return clip;
 	}
 
-	public AudioInputStream getAudioInputStream(File file)
-			throws UnsupportedAudioFileException, IOException {
+	public AudioInputStream getAudioInputStream(File file) throws UnsupportedAudioFileException, IOException {
 		return AudioSystem.getAudioInputStream(file);
 	}
 
-	public Map<Integer, ChannelInfo> getTargetLines(){
+	public Map<Integer, ChannelInfo> getTargetLines() {
 		HashMap<Integer, ChannelInfo> channels = new HashMap<Integer, ChannelInfo>();
-		for(Mixer.Info mixerInfo : AudioSystem.getMixerInfo()){
-			for(Line.Info lineInfo : AudioSystem.getMixer(mixerInfo).getTargetLineInfo(getTargetLineInfo())){
+		for (Mixer.Info mixerInfo : AudioSystem.getMixerInfo()) {
+			for (Line.Info lineInfo : AudioSystem.getMixer(mixerInfo).getTargetLineInfo(this.getTargetLineInfo())) {
 				ChannelInfo channel = new ChannelInfo(channels.size(), mixerInfo, lineInfo);
 				channels.put(channel.getId(), channel);
 			}
@@ -53,13 +54,13 @@ public class AudioSystemProvider {
 	}
 
 	public Line getLine(Line.Info info) throws LineUnavailableException {
-		if(info == null){
-			info = getTargetLineInfo();
+		if (info == null) {
+			info = this.getTargetLineInfo();
 		}
 		return AudioSystem.getLine(info);
 	}
-	
-	private Info getTargetLineInfo(){
-		return new DataLine.Info(TargetDataLine.class, appConfig.getAudioFormat());
+
+	private Info getTargetLineInfo() {
+		return new DataLine.Info(TargetDataLine.class, this.appConfig.getAudioFormat());
 	}
 }

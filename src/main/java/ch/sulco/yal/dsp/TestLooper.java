2bd7de92c107c8fed937be89609b2e3a75d35462
Roman Studer <roman.studer@ergon.ch>
2015-10-07 09:27:55 +0200

    fix test looper

--- src/main/java/ch/sulco/yal/dsp/TestLooper.java
+++ src/main/java/ch/sulco/yal/dsp/TestLooper.java
@@ -1,17 +1,19 @@
 package ch.sulco.yal.dsp;
 
+import com.google.inject.Guice;
+
 import ch.sulco.yal.Application;
+import ch.sulco.yal.PostConstructModule;
+import ch.sulco.yal.YalModule;
 
 public class TestLooper {
 	public static void main(String[] args) throws Exception {
-		Application application = new Application();
+		Application application = Guice.createInjector(new YalModule(), new PostConstructModule()).getInstance(Application.class);
+		application.start();
 		System.out.println(application.getAudioProcessor().getLoopLength());
+		Thread.sleep(500);
 		application.getAudioProcessor().play();
-		Thread.sleep(10000);
+		Thread.sleep(1000);
 		application.getAudioProcessor().loop();
-		
-		System.out.println(application.getAudioProcessor().getSampleVolume(0));
-		System.out.println(application.getAudioProcessor().isSampleMute(0));
-		System.out.println(application.getAudioProcessor().getLoopLength());
 	}
 }

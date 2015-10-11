3c7fe725aceb624ff71fa44035758e35d7bc7e93
Marco <marco.zimm@gmail.com>
2015-10-11 23:21:47 +0200

    print out audio setup at startup

--- src/main/java/ch/sulco/yal/dsp/audio/onboard/AudioSystemProvider.java
+++ src/main/java/ch/sulco/yal/dsp/audio/onboard/AudioSystemProvider.java
@@ -10,9 +10,14 @@ import javax.inject.Singleton;
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.BooleanControl;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.CompoundControl;
+import javax.sound.sampled.Control;
 import javax.sound.sampled.DataLine;
 import javax.sound.sampled.DataLine.Info;
+import javax.sound.sampled.EnumControl;
+import javax.sound.sampled.FloatControl;
 import javax.sound.sampled.Line;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.Mixer;
@@ -50,6 +55,36 @@ public class AudioSystemProvider {
 	}
 
 	public List<Channel> getChannels() {
+		try {
+			for (Mixer.Info thisMixerInfo : AudioSystem.getMixerInfo()) {
+				System.out.println("Mixer: " + thisMixerInfo.getDescription() + " [" + thisMixerInfo.getName() + "]");
+				Mixer thisMixer = AudioSystem.getMixer(thisMixerInfo);
+				for (Line.Info thisLineInfo : thisMixer.getSourceLineInfo()) {
+					if (thisLineInfo.getLineClass().getName().equals("javax.sound.sampled.Port")) {
+						Line thisLine = thisMixer.getLine(thisLineInfo);
+						thisLine.open();
+						System.out.println("  Source Port: " + thisLineInfo.toString());
+						for (Control thisControl : thisLine.getControls()) {
+							System.out.println(AnalyzeControl(thisControl));
+						}
+						thisLine.close();
+					}
+				}
+				for (Line.Info thisLineInfo : thisMixer.getTargetLineInfo()) {
+					if (thisLineInfo.getLineClass().getName().equals("javax.sound.sampled.Port")) {
+						Line thisLine = thisMixer.getLine(thisLineInfo);
+						thisLine.open();
+						System.out.println("  Target Port: " + thisLineInfo.toString());
+						for (Control thisControl : thisLine.getControls()) {
+							System.out.println(AnalyzeControl(thisControl));
+						}
+						thisLine.close();
+					}
+				}
+			}
+		} catch (LineUnavailableException e) {
+			e.printStackTrace();
+		}
 		long id = 0;
 		List<Channel> channels = new ArrayList<>();
 		Mixer mixer = AudioSystem.getMixer(null);
@@ -72,6 +107,29 @@ public class AudioSystemProvider {
 			id++;
 		}
 		return channels;
+	}
+	
+	public String AnalyzeControl(Control thisControl) {
+		String type = thisControl.getType().toString();
+		if (thisControl instanceof BooleanControl) {
+			return "    Control: " + type + " (boolean)";
+		}
+		if (thisControl instanceof CompoundControl) {
+			System.out.println("    Control: " + type + " (compound - values below)");
+			String toReturn = "";
+			for (Control children : ((CompoundControl) thisControl).getMemberControls()) {
+				toReturn += "  " + AnalyzeControl(children) + "\n";
+			}
+			return toReturn.substring(0, toReturn.length() - 1);
+		}
+		if (thisControl instanceof EnumControl) {
+			return "    Control:" + type + " (enum: " + thisControl.toString() + ")";
+		}
+		if (thisControl instanceof FloatControl) {
+			return "    Control: " + type + " (float: from " + ((FloatControl) thisControl).getMinimum() + " to "
+					+ ((FloatControl) thisControl).getMaximum() + ")";
+		}
+		return "    Control: unknown type";
 	}
 
 	public Line getLine(Line.Info info) throws LineUnavailableException {
